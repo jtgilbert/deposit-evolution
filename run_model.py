ec4b26e7edecd9870d536b4d3a2d5007ea647911
Jordan <jtilbert89@gmail.com>
2022-12-26 17:16:09 -0700

    starting

---
+++ run_model.py
@@ -0,0 +1,142 @@
+from model.deposit_evolution import DepositEvolution
+
+boundary_conditions = {
+    "upstream_slope": None,
+    "flow_scale": None,
+    "upstream_gsd": {
+        2: None,
+        1: None,
+        0: None,
+        -1: None,
+        -2: None,
+        -2.5: None,
+        -3: None,
+        -3.5: None,
+        -4: None,
+        -4.5: None,
+        -5: None,
+        -5.5: None,
+        -6: None,
+        -7: None,
+        -7.5: None,
+        -8: None,
+        -8.5: None,
+        -9: None
+    }
+}
+
+reaches = {
+    "upstream": {
+        "flow_scale": None,
+        "elevation": None,
+        "width": None,
+        "height": None,
+        "gsd": {
+            2: None,
+            1: None,
+            0: None,
+            -1: None,
+            -2: None,
+            -2.5: None,
+            -3: None,
+            -3.5: None,
+            -4: None,
+            -4.5: None,
+            -5: None,
+            -5.5: None,
+            -6: None,
+            -7: None,
+            -7.5: None,
+            -8: None,
+            -8.5: None,
+            -9: None
+        },
+        "Qs_in": None,
+        "Qs_out": None
+    },
+    "deposit_upstream": {
+        "flow_scale": None,
+        "elevation": None,
+        "width": None,
+        "height": None,
+        "gsd": {
+            2: None,
+            1: None,
+            0: None,
+            -1: None,
+            -2: None,
+            -2.5: None,
+            -3: None,
+            -3.5: None,
+            -4: None,
+            -4.5: None,
+            -5: None,
+            -5.5: None,
+            -6: None,
+            -7: None,
+            -7.5: None,
+            -8: None,
+            -8.5: None,
+            -9: None
+        },
+        "Qs_in": None,
+        "Qs_out": None
+    },
+    "deposit_downstream": {
+        "flow_scale": None,
+        "elevation": None,
+        "width": None,
+        "height": None,
+        "gsd": {
+            2: None,
+            1: None,
+            0: None,
+            -1: None,
+            -2: None,
+            -2.5: None,
+            -3: None,
+            -3.5: None,
+            -4: None,
+            -4.5: None,
+            -5: None,
+            -5.5: None,
+            -6: None,
+            -7: None,
+            -7.5: None,
+            -8: None,
+            -8.5: None,
+            -9: None
+        },
+        "Qs_in": None,
+        "Qs_out": None
+    },
+    "downstream": {
+        "flow_scale": None,
+        "elevation": None,
+        "width": None,
+        "height": None,
+        "gsd": {
+            2: None,
+            1: None,
+            0: None,
+            -1: None,
+            -2: None,
+            -2.5: None,
+            -3: None,
+            -3.5: None,
+            -4: None,
+            -4.5: None,
+            -5: None,
+            -5.5: None,
+            -6: None,
+            -7: None,
+            -7.5: None,
+            -8: None,
+            -8.5: None,
+            -9: None
+        },
+        "Qs_in": None,
+        "Qs_out": None
+    },
+    "deposit_volume": None
+}
