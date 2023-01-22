efcaec0bfa2368525e5726814f701be16ed1f966
Jordan <jtilbert89@gmail.com>
2023-01-21 17:53:18 -0700

    testing on 7 yr flow

--- one_hour_discharge.py
+++ one_hour_discharge.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv('Inputs/Woods_Q.csv')
+df = pd.read_csv('Inputs/WoodsQ_2012_2019_15min.csv')
 
 out_datetime = []
 out_q = []
@@ -8,9 +8,9 @@ out_q = []
 for i in df.index:
     if ':00' in df.loc[i, 'Datetime']:
         out_datetime.append(df.loc[i, 'Datetime'])
-        out_q.append((df.loc[i, 'Q']+df.loc[i+1, 'Q']+df.loc[i+2, 'Q']+df.loc[i+3, 'Q'])/4)
+        out_q.append(((df.loc[i, 'Q']+df.loc[i+1, 'Q']+df.loc[i+2, 'Q']+df.loc[i+3, 'Q'])/4)) # halving discharge
 
 data = {'Datetime': out_datetime, 'Q': out_q}
 out_df = pd.DataFrame(data=data)
 
-out_df.to_csv('Inputs/Woods_Q_1hr.csv')
+out_df.to_csv('Inputs/Woods_Q_2012_2019_1hr.csv')
